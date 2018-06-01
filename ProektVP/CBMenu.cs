2f5c6adf453b6f85e1c83d5a5d86dddb1e54f910
Elena <elena.naunova@icloud.com>
2018-06-01 16:26:45 +0200

    add CB menu

---
+++ ProektVP/CBMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProektVP
+{
+    public partial class CBMenu : Form
+    {
+        public CBMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string rules = "1. Click the ball before it falls into the water." +
+                "\n" + "2. Each click is one point.\n3. Only once, you can change 20 points for one more chance." +
+                "\n\n                    Try to reach Level 10. GOOD LUCK!";
+            MessageBox.Show(rules, "Rules of the game", MessageBoxButtons.OK);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CBGame cbGame = new CBGame();
+            cbGame.ShowDialog();
+        }
+    }
+}
