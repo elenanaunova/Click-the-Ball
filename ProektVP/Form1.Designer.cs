4b53eeede1c07c03d61e9934a608227c6600ee28
ivonaefremova <ivonae9706@gmail.com>
2018-06-07 17:22:34 +0200

    add picture

--- ProektVP/Form1.Designer.cs
+++ ProektVP/Form1.Designer.cs
@@ -28,73 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.choice1 = new System.Windows.Forms.Button();
             this.choice2 = new System.Windows.Forms.Button();
+            this.choiceStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Cyan;
-            this.label1.Location = new System.Drawing.Point(106, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(235, 50);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Choose game";
-            // 
-            // choice1
-            // 
-            this.choice1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.choice1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.choice1.FlatAppearance.BorderSize = 5;
-            this.choice1.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.choice1.ForeColor = System.Drawing.Color.DarkTurquoise;
-            this.choice1.Location = new System.Drawing.Point(124, 127);
-            this.choice1.Name = "choice1";
-            this.choice1.Size = new System.Drawing.Size(226, 36);
-            this.choice1.TabIndex = 1;
-            this.choice1.Text = "Click the ball";
-            this.choice1.UseVisualStyleBackColor = false;
-            this.choice1.Click += new System.EventHandler(this.choice1_Click);
             // 
             // choice2
             // 
-            this.choice2.BackColor = System.Drawing.SystemColors.InfoText;
-            this.choice2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.choice2.BackColor = System.Drawing.SystemColors.Highlight;
             this.choice2.FlatAppearance.BorderSize = 0;
-            this.choice2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
-            this.choice2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.choice2.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.choice2.ForeColor = System.Drawing.Color.DarkTurquoise;
-            this.choice2.Location = new System.Drawing.Point(124, 190);
+            this.choice2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.choice2.Font = new System.Drawing.Font("Jokerman", 12.7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choice2.ForeColor = System.Drawing.Color.White;
+            this.choice2.Location = new System.Drawing.Point(393, 203);
             this.choice2.Name = "choice2";
-            this.choice2.Size = new System.Drawing.Size(226, 36);
-            this.choice2.TabIndex = 2;
-            this.choice2.Text = "Move the ball";
+            this.choice2.Size = new System.Drawing.Size(81, 46);
+            this.choice2.TabIndex = 0;
+            this.choice2.Text = "EXIT";
+            this.choice2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
             this.choice2.UseVisualStyleBackColor = false;
+            this.choice2.Click += new System.EventHandler(this.choice2_Click);
+            // 
+            // choiceStart
+            // 
+            this.choiceStart.BackColor = System.Drawing.SystemColors.Highlight;
+            this.choiceStart.FlatAppearance.BorderSize = 0;
+            this.choiceStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.choiceStart.Font = new System.Drawing.Font("Jokerman", 12F);
+            this.choiceStart.ForeColor = System.Drawing.Color.White;
+            this.choiceStart.Location = new System.Drawing.Point(20, 184);
+            this.choiceStart.Name = "choiceStart";
+            this.choiceStart.Size = new System.Drawing.Size(96, 45);
+            this.choiceStart.TabIndex = 1;
+            this.choiceStart.Text = "START";
+            this.choiceStart.UseVisualStyleBackColor = false;
+            this.choiceStart.Click += new System.EventHandler(this.choiceStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.BackgroundImage = global::ProektVP.Properties.Resources.bukvi;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(119, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(287, 111);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::ProektVP.Properties.Resources.pozadina1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(167, 213);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(194, 169);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.MenuText;
-            this.ClientSize = new System.Drawing.Size(483, 386);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::ProektVP.Properties.Resources.pozadina;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(492, 493);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.choiceStart);
             this.Controls.Add(this.choice2);
-            this.Controls.Add(this.choice1);
-            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.MaximumSize = new System.Drawing.Size(510, 540);
+            this.MinimumSize = new System.Drawing.Size(510, 540);
             this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button choice1;
         private System.Windows.Forms.Button choice2;
+        private System.Windows.Forms.Button choiceStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
