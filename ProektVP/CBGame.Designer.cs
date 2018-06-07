f55c487042b32f65a146dfb00103d843864490de
Elena <elena.naunova@icloud.com>
2018-06-07 17:07:05 +0200

    change picture location

--- ProektVP/CBGame.Designer.cs
+++ ProektVP/CBGame.Designer.cs
@@ -29,18 +29,33 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::ProektVP.Properties.Resources.click;
-            this.pictureBox1.Location = new System.Drawing.Point(-8, 412);
+            this.pictureBox1.InitialImage = global::ProektVP.Properties.Resources.click;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 400);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(556, 90);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(551, 81);
+            this.pictureBox1.Size = new System.Drawing.Size(556, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Image = global::ProektVP.Properties.Resources.click;
+            this.label1.Location = new System.Drawing.Point(463, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 34);
+            this.label1.TabIndex = 1;
             // 
             // CBGame
             // 
@@ -48,17 +63,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(536, 483);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(554, 530);
             this.Name = "CBGame";
             this.Text = "Good luck";
             this.Load += new System.EventHandler(this.CBGame_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CBGame_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CBGame_MouseClick);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
