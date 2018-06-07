e0f4f37a04317c618f3a2940e9c1f05dcdd70ef4
Elena <elena.naunova@icloud.com>
2018-06-07 17:10:33 +0200

    change Resources

--- ProektVP/Properties/Resources.Designer.cs
+++ ProektVP/Properties/Resources.Designer.cs
@@ -121,5 +121,35 @@ namespace ProektVP.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap game {
+            get {
+                object obj = ResourceManager.GetObject("game", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pozadina {
+            get {
+                object obj = ResourceManager.GetObject("pozadina", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pozadina1 {
+            get {
+                object obj = ResourceManager.GetObject("pozadina1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
