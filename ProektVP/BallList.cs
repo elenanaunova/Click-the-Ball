5b50fa5d1d4928c03e4ec99829e695a8c4208e3b
Elena <elena.naunova@icloud.com>
2018-06-07 17:05:05 +0200

    add functions

--- ProektVP/BallList.cs
+++ ProektVP/BallList.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ProektVP
 {
     public class BallList
@@ -14,23 +15,165 @@ namespace ProektVP
         {
             this.list = new List<Ball>();
         }
+   
         public void Draw(Graphics g)
-        {
-            foreach(Ball b in list)
-            {
-                b.Draw(g);
-            }
-        }
-        public void Move(float x, float y, int left, int top, int width, int height)
         {
             foreach (Ball b in list)
             {
-                b.Move(x, y, left, top, width, height);
+                if(b.color==Color.Red)
+                    b.Draw(g);
             }
+            foreach (Ball b in list)
+            {
+                if (b.color != Color.Red)
+                    b.Draw(g);
+            }
+        }
+        public void MoveBeforeClick(int left, int top, int width, int height)
+        {
+            foreach (Ball b in list)
+            {
+                if (b.color == Color.Red &&b.RADIUS!=35)
+                {
+                    b.MoveDown(height);
+                       
+                }
+                   
+                else
+                    b.MoveClicked(left, top, width, height);       
+            }
+        }
+        public bool gameOver(int height)
+        {
+            foreach(Ball b in list)
+            {
+                if (b.gameOver(height))
+                    return true;
+            }
+            return false;
+        }
+        public void MoveClick(int left, int top, int width, int height)
+        {
+            foreach (Ball b in list)
+            {
+                    b.MoveClicked(left, top, width, height);
+            }
+                
         }
         public void AddBall(Ball b)
         {
             list.Add(b);
+        }
+        public bool isClickedRed(int x,int y)
+        {
+            foreach(Ball b in list)
+            {
+               if(b.color==Color.Red && b.RADIUS!=35)
+                if(b.isClicked(x, y))
+                        return true;
+            }
+            return false;
+        }
+        public bool isClickedBlue(int x,int y)
+        {
+            foreach (Ball b in list)
+            {
+                if (b.isClicked(x, y))
+                {
+                    if (b.color == SystemColors.Highlight)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+        public void MoveUp()
+        {
+            foreach (Ball b in list)
+            {
+                if (b.color == Color.Red)
+                    b.MoveUp();
+            }
+        }
+        public void deleteRedClick(int x,int y)
+        {
+            for(int i = list.Count - 1; i >= 0; i--)
+            {
+                Ball b = list.ElementAt(i);
+                if (b.color == Color.Red && b.RADIUS!=35)
+                {
+                    list.Remove(b);
+                    list.Add(new Ball(30, x, y, 15, Color.Red));
+                }
+
+            }
+
+        }
+
+        public void deleteWrongRed()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                Ball b = list.ElementAt(i);
+                if (b.color == Color.Red && b.RADIUS == 35)
+                    list.Remove(b);
+            }
+        }
+
+        public void deleteBlue()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                Ball b = list.ElementAt(i);
+                if (b.color == SystemColors.Highlight)
+                    list.Remove(b);
+            }
+        }
+
+        public Point deleteRed()
+        {
+            Point k=new Point(0,0);
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                
+                Ball b = list.ElementAt(i);
+                if (b.color == Color.Red && b.RADIUS !=35)
+                {
+                    k = new Point((int)b.X, (int)b.Y);
+                    list.Remove(b);
+                }               
+            }
+            return k;
+        }
+
+        public void changeBlue()
+        {
+
+            foreach (Ball b in list)
+                b.promeniRadiusPlavo();
+        }
+
+
+        public void promeniRadius()
+        {
+            foreach (Ball b in list)
+                b.promeniRadius();
+        }
+        public void promeniBrzina(bool zabrzaj)
+        {
+            Point p = deleteRed();
+            if (zabrzaj)
+            {
+                Ball b = new Ball(30, p.X, p.Y, 35, Color.Red);
+                list.Add(b);
+            }
+            else
+            {
+                Ball b = new Ball(30, p.X, p.Y, 10, Color.Red);
+                list.Add(b);
+            }
         }
     }
 }
